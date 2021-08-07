2d0993ef8439dc86350248bd1b18ab90677d51fb
supremetakoyaki <marcelo.alvarez.e@usach.cl>
2021-08-07 03:52:56 -0400

    add DPI scale to misc.editor

--- Forms/MiscEditor.Designer.cs
+++ Forms/MiscEditor.Designer.cs
@@ -70,8 +70,8 @@ namespace Scramble.Forms
             // 
             // MiscEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(321, 113);
             this.Controls.Add(this.DlcGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
