bfc140654b992c4f7c88b0aeae7402787816c35d
supremetakoyaki <marcelo.alvarez.e@usach.cl>
2021-08-14 21:38:07 -0400

    Update version

--- Forms/ScrambleForm.Designer.cs
+++ Forms/ScrambleForm.Designer.cs
@@ -188,7 +188,7 @@
             this.AboutLabel.Name = "AboutLabel";
             this.AboutLabel.Size = new System.Drawing.Size(76, 15);
             this.AboutLabel.TabIndex = 0;
-            this.AboutLabel.Text = "v0.7.2";
+            this.AboutLabel.Text = "v0.8";
             this.AboutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.AboutLabel.Click += new System.EventHandler(this.AboutLabel_Click);
             // 
