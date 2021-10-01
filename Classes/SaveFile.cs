1807f6db615168e9a9244a6dc1f5cfde7ea6591a
T <marcelo.alvarez.e@usach.cl>
2021-09-30 21:40:26 -0300

    Acknowledge the PC version

--- Classes/SaveFile.cs
+++ Classes/SaveFile.cs
@@ -10,18 +10,26 @@ namespace Scramble.Classes
         private readonly Dictionary<int, SaveData> SaveSlots;
         private readonly GlobalData GlobalData;
 
+        private const int PC_KEY_LENGTH = 32;
         private const int GLOBAL_DATA_LENGTH = 619;
 
         private const int INIT_OFFSET = 619;
         private const int SLOT_LENGTH = 319989;
 
         private const int SAVE_LENGTH = 3200512;
+        private const int PC_SAVE_LENGTH = 3201572;
 
-        private const int PS4_ENCRYPTED_LENGTH = 6422528;
+        public bool IsPcVersion = false;
 
         public SaveFile(string FilePath, byte[] File, out byte Result)
         {
-            if (File.Length != SAVE_LENGTH)
+            if (File.Length == PC_SAVE_LENGTH)
+            {
+                //IsPcVersion = true;
+                Result = 3;
+                return;
+            }
+            else if (File.Length != SAVE_LENGTH)
             {
                 Result = 0;
                 return;
@@ -29,24 +37,32 @@ namespace Scramble.Classes
 
             this.FilePath = FilePath;
 
-            byte[] FirstData = new byte[GLOBAL_DATA_LENGTH];
-            Array.Copy(File, 0, FirstData, 0, GLOBAL_DATA_LENGTH);
-            GlobalData = new GlobalData(FirstData);
-
-            SaveSlots = new Dictionary<int, SaveData>();
-
-            int CurrentPointer = INIT_OFFSET;
-            for (int i = 0; i < 10; i++)
+            if (IsPcVersion)
             {
-                byte[] SlotData = new byte[SLOT_LENGTH];
-                Array.Copy(File, CurrentPointer, SlotData, 0, SLOT_LENGTH);
-
-                SaveSlots.Add(i, new SaveData(i, SlotData));
-
-                CurrentPointer += SLOT_LENGTH;
+                // PC version uses encryption.
+                Result = 1;
             }
+            else
+            {
+                byte[] FirstData = new byte[GLOBAL_DATA_LENGTH];
+                Array.Copy(File, 0, FirstData, 0, GLOBAL_DATA_LENGTH);
+                GlobalData = new GlobalData(FirstData);
 
-            Result = 1;
+                SaveSlots = new Dictionary<int, SaveData>();
+
+                int CurrentPointer = INIT_OFFSET;
+                for (int i = 0; i < 10; i++)
+                {
+                    byte[] SlotData = new byte[SLOT_LENGTH];
+                    Array.Copy(File, CurrentPointer, SlotData, 0, SLOT_LENGTH);
+
+                    SaveSlots.Add(i, new SaveData(i, SlotData));
+
+                    CurrentPointer += SLOT_LENGTH;
+                }
+
+                Result = 1;
+            }
         }
 
         public SaveData GetSaveSlot(int Id)
